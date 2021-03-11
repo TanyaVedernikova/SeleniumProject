8a862e387c9d917de6e15d62c54a43b8592e4beb
TanyaVedernikova <65467774+TanyaVedernikova@users.noreply.github.com>
2021-03-11 10:12:34 +0500

    Add files via upload
    new version

--- main.py
+++ main.py
@@ -2,25 +2,29 @@ from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 
 def main():
-    name_of_animal = []
+    name_of_animal_and_count_of_metrs = {}
     driver = webdriver.Chrome()
-    count_of_metrs = []
     driver.get("https://neal.fun/deep-sea/")
     elem = driver.find_elements_by_class_name("eNQuho")
-    for i in elem:
-        name_of_animal.append(i.text)
+    for i in range(len(elem)):
         actions = ActionChains(driver)
-        actions.move_to_element(i).perform()
+        actions.move_to_element(elem[i]).perform()
         metrs = driver.find_elements_by_class_name('hobqQZ')
         for m in metrs:
-            count_of_metrs.append(m.text)
-        
+            name_of_animal_and_count_of_metrs.update({elem[i].text : m.text})
+            if  name_of_animal_and_count_of_metrs.get(elem[i]) == name_of_animal_and_count_of_metrs.get(elem[i-1]):
+                actions = ActionChains(driver)
+                header = driver.find_elements_by_class_name("jZUSDr")
+                for h in header:
+                    actions.move_to_element(h).perform()
+                    actions = ActionChains(driver)
+                    actions.move_to_element(elem[i]).perform()
+                    name_of_animal_and_count_of_metrs.update({elem[i].text: m.text})
 
 
-    print(count_of_metrs)
-    print(len(name_of_animal))
-    print(len(count_of_metrs))
-    print(name_of_animal)
+
+    print(name_of_animal_and_count_of_metrs)
+    print(len(name_of_animal_and_count_of_metrs))
 
 
 
